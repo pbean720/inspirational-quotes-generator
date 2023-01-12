0f9e198962c9bc741d1591ef37e4376f43fc1715
pbean720 <79056706+pbean720@users.noreply.github.com>
2023-01-12 15:43:27 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 //////////////// TO DO LIST //////////////////////
-// ADD COMMENTS FOR EVERYTHING
 
 var pageTitle = document.getElementById("pageTitle")
 var quote = document.getElementById("quote")
@@ -9,7 +8,7 @@ var spanLines = document.getElementsByTagName("span")
 var btns = document.getElementsByClassName("material-symbols-outlined")
 var savebtn = document.getElementById("savebtn")
 
-backgrounds = 
+var backgrounds = 
             [// [bg img,         quote font,                       h1 and author font,               function]
                 ["imgs/1.png",   "'Noto Serif Display', serif",    "'Lato', sans-serif",             bgOne], 
                 ["imgs/2.png",   "'Fraunces', serif",              "Arial, Helvetica, sans-serif",   bgTwo], 
@@ -22,21 +21,25 @@ var lastBg = 0
 var max = backgrounds.length;
 console.log(max)
 
+// PRODUCES A RANDOM INTEGER FOR THE LENGTH OF THE NUMBER OF BACKGROUNDS THERE ARE
 function getRandomInt(max) {
     var randInt = (Math.floor(Math.random() * max))
     return randInt
 }
 
+// EVERYTIME THE REFRESH BUTTON IS CLICKED, A RANDOM INT IS TAKEN IN OF THE LENGTH OF BACKGROUND AND CHANGES THE STYLE ELEMENTS ON THE PAGE
+// AND THE TEXT TO PRODUCE A NEW QUOTE AND QUOTE AUTHOR ON CLICK
 document.getElementById("refreshbtn").addEventListener("click", function () {
     bgIndex = getRandomInt(max)
-    console.log(bgIndex)
 
+    // THE SAME BACKGROUND IMAGE WILL NEVER REAPPEAR TWICE IN A ROW
     while (bgIndex == lastBg) {
         bgIndex = getRandomInt(max)
     }
 
     lastBg = bgIndex
 
+    // RESETS THE STYLE ELEMENTS THAT WERE CHANGED IN THE UNIQUE BACKGROUND FUNCTIONS
     cremeBg.style.backgroundColor = ""
     quote.style.fontStyle = "";
     quote.style.fontWeight = ""
@@ -45,25 +48,36 @@ document.getElementById("refreshbtn").addEventListener("click", function () {
     pageTitle.style.color = ""
     quote.style.color = "";
     author.style.color = ""
+
+    // MAKES THE BORDER/ SPAN LINES IN THE BACKGROUNDS OF BG 1 WHITE, THE LINES IN THE BACKGROUND OF BG 3 BLACK,
+    // AND TRANSPARENT IN THE BACKGROUND OF BGS 2, 4, AND 5
     lines(bgIndex)
+
+    // CHANGES THE COLOR OF THE GOOGLE ICONS/ BUTTONS FOR EACH BACKGROUND
     btnColor(bgIndex)
 
-
+    // USES THE IMAGE LINKS AND FONTS LISTED IN THE BACKGROUNDS ARRAY TO CHANGE THE BACKGROUND AND FONTS ON THE PAGE EVERYTIME THE REFRESH BUTTON IS CLICKED
     document.body.style.backgroundImage = "url('" + backgrounds[bgIndex][0] + "')";
-    console.log(backgrounds[bgIndex][1])
-    console.log(backgrounds[bgIndex][2])
     pageTitle.style.fontFamily = backgrounds[bgIndex][2];
     quote.style.fontFamily = backgrounds[bgIndex][1];
     author.style.fontFamily = backgrounds[bgIndex][2];
 
+    // MAKES THE BORDER/ SPAN LINES IN THE BACKGROUND OF BACKGROUND 1 WHITE, THE LINES IN THE BACKGROUND OF BACKGROUND 3 BLACK,
+    // AND TRANSPARENT IN THE BACKGROUND OF BACKGROUNDS 2, 4, AND 5
     backgrounds[bgIndex][3]()
+
+    // CALLS FETCHQUOTE() FUNCTION SO THAT EVERYTIME THE REFRESH BUTTON IS CLICKED, A NEW RANDOM QUOTE WILL BE DISPLAYED ON THE SCREEN
     fetchQuote()
 })
 
 
+// THE FOLLOWING FIVE FUNCTIONS CHANGE THE PAGES STYLE ELEMENTS TO MAKE EACH OF THE FIVE BACKGROUNDS UNIQUE
+
+// changes style elements for background 1
 function bgOne() {
 }
 
+// changes style elements for background 2
 function bgTwo() {
  quote.style.fontStyle = "italic";
  quote.style.fontWeight = "bold"
@@ -73,6 +87,7 @@ function bgTwo() {
  author.style.color = "#737373"
 }
 
+// changes style elements for background 3
 function bgThree() {
 quote.style.fontWeight = "bold"
  pageTitle.style.color = "#323232"
@@ -81,6 +96,7 @@ quote.style.fontWeight = "bold"
 
 }
 
+// changes style elements for background 4
 function bgFour() {
  cremeBg.style.backgroundColor = "#faebd7"
  pageTitle.style.color = "#896244"
@@ -91,6 +107,7 @@ function bgFour() {
 
 }
 
+// changes style elements for background 5
 function bgFive() {
  pageTitle.style.color = "brown"
  quote.style.color = "brown";
@@ -98,6 +115,8 @@ function bgFive() {
 cremeBg.style.backgroundColor = "#faebd7"
 }
 
+// MAKES THE BORDER/ SPAN LINES IN THE BACKGROUND OF BACKGROUND 1 WHITE, THE LINES IN THE BACKGROUND OF BACKGROUND 3 BLACK,
+// AND TRANSPARENT IN THE BACKGROUND OF BACKGROUNDS 2, 4, AND 5
 function lines(bgIndex) {
 
         console.log(spanLines)
@@ -137,6 +156,7 @@ function lines(bgIndex) {
 
 }
 
+// CHANGES THE COLOR OF THE GOOGLE ICONS/ BUTTONS FOR EACH BACKGROUND
 function btnColor(bgIndex) {
     for (var i = 0, max = btns.length; i < max; i++) {
         if (bgIndex == 0) {
@@ -154,6 +174,7 @@ function btnColor(bgIndex) {
     }
 }
 
+// FETCHES THE INSPIRATIONAL QUOTES API AND DISPLAYS THE QUOTE AND ITS AUTHOR ON THE PAGE
 function fetchQuote() {
     fetch("http://quotable.io/random")
         .then(function (response){
